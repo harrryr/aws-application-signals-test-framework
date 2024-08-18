c95edc727977f40ff2d20c49a371d91ef2b5ae67
harryryu <harryryu@amazon.com>
2024-08-17 21:29:21 -0700

    Test

--- terraform/java/ec2/asg/main.tf
+++ terraform/java/ec2/asg/main.tf
@@ -143,7 +143,7 @@ resource "aws_launch_configuration" "launch_configuration" {
     npm install
 
     tmux new -s traffic-generator -d
-    tmux send-keys -t traffic-generator "export MAIN_ENDPOINT=\"localhost:8080\"" C-m
+    tmux send-keys -t traffic-generator "export MAIN_ENDPOINT=\"localhost:8085\"" C-m
     tmux send-keys -t traffic-generator "export REMOTE_ENDPOINT=\"${aws_instance.remote_service_instance.private_ip}\"" C-m
     tmux send-keys -t traffic-generator "export ID=\"${var.test_id}\"" C-m
     tmux send-keys -t traffic-generator "npm start" C-m
