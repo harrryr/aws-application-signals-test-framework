9dde6a6e01991b62e1ac962aa3d811ceb46e14fb
harryryu <harryryu@amazon.com>
2024-08-26 15:43:52 -0700

    Test

--- .github/workflows/util/setup-k8s.sh
+++ .github/workflows/util/setup-k8s.sh
@@ -87,33 +87,33 @@ function run_k8s_master() {
 
   # set up kubeadmin
   ssh -o StrictHostKeyChecking=no -i "${KEY_NAME}.pem" ec2-user@$master_ip << EOF
-    sudo yum update -y && sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm && \
-    sudo yum install docker tmux git vim -y && sudo usermod -aG docker ec2-user && \
-    sudo systemctl enable docker && sudo systemctl start docker && \
-    sudo containerd config default > config.toml && \
-    sudo cp config.toml /etc/containerd/config.toml && \
-    sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/' /etc/containerd/config.toml && \
-    sudo sed -i 's/systemd_cgroup \= true/systemd_cgroup \= true/' /etc/containerd/config.toml && \
-    sudo systemctl restart containerd && sleep 20 && \
-    sudo setenforce 0 && sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config && \
-    echo -e "[kubernetes]\nname=Kubernetes\nbaseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/\nenabled=1\ngpgcheck=1\ngpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key\nexclude=kubelet kubeadm kubectl cri-tools kubernetes-cni" | sudo tee /etc/yum.repos.d/kubernetes.repo && \
-    sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes && \
-    sudo systemctl enable --now kubelet && sudo systemctl restart kubelet && sleep 30 && \
-    sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=$master_private_ip --apiserver-cert-extra-sans=$worker_private_ip && \
-    mkdir -p \$HOME/.kube && \
-    sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config && \
-    sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config && \
-    sleep 120 && \
-    curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml -O && \
-    kubectl apply -f calico.yaml && sleep 60 && \
-    sudo cd \$HOME && \
-    sudo cp /etc/kubernetes/pki/apiserver.crt apiserver.crt && \
-    sudo cp /etc/kubernetes/pki/apiserver.key apiserver.key && \
-    sudo chmod +r apiserver.key && \
-    sudo kubeadm token create --print-join-command > join-cluster.sh && \
-    sudo chmod +x join-cluster.sh && \
-    echo "tlsCertFile: /etc/kubernetes/pki/apiserver.crt" | sudo tee -a /var/lib/kubelet/config.yaml && \
-    echo "tlsPrivateKeyFile: /etc/kubernetes/pki/apiserver.key" | sudo tee -a /var/lib/kubelet/config.yaml && \
+    sudo yum update -y && sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo yum install docker tmux git vim -y && sudo usermod -aG docker ec2-user
+    sudo systemctl enable docker && sudo systemctl start docker
+    sudo containerd config default > config.toml
+    sudo cp config.toml /etc/containerd/config.toml
+    sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/' /etc/containerd/config.toml
+    sudo sed -i 's/systemd_cgroup \= true/systemd_cgroup \= true/' /etc/containerd/config.toml
+    sudo systemctl restart containerd && sleep 20
+    sudo setenforce 0 && sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+    echo -e "[kubernetes]\nname=Kubernetes\nbaseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/\nenabled=1\ngpgcheck=1\ngpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key\nexclude=kubelet kubeadm kubectl cri-tools kubernetes-cni" | sudo tee /etc/yum.repos.d/kubernetes.repo
+    sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+    sudo systemctl enable --now kubelet && sudo systemctl restart kubelet && sleep 30
+    sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=$master_private_ip --apiserver-cert-extra-sans=$worker_private_ip
+    mkdir -p \$HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
+    sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config
+    sleep 120
+    curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml -O
+    kubectl apply -f calico.yaml && sleep 60
+    sudo cd \$HOME
+    sudo cp /etc/kubernetes/pki/apiserver.crt apiserver.crt
+    sudo cp /etc/kubernetes/pki/apiserver.key apiserver.key
+    sudo chmod +r apiserver.key
+    sudo kubeadm token create --print-join-command > join-cluster.sh
+    sudo chmod +x join-cluster.sh
+    echo "tlsCertFile: /etc/kubernetes/pki/apiserver.crt" | sudo tee -a /var/lib/kubelet/config.yaml
+    echo "tlsPrivateKeyFile: /etc/kubernetes/pki/apiserver.key" | sudo tee -a /var/lib/kubelet/config.yaml
     sudo systemctl restart kubelet
 EOF
 
@@ -136,23 +136,23 @@ function run_k8s_worker() {
 
   # set up kubeadmin
   ssh -o StrictHostKeyChecking=no -i "${KEY_NAME}.pem" ec2-user@$worker_ip << EOF
-    sudo yum update -y && sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm && \
-    sudo yum install docker tmux git vim -y && sudo usermod -aG docker ec2-user && \
+    sudo yum update -y && sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo yum install docker tmux git vim -y && sudo usermod -aG docker ec2-user
     sudo systemctl enable docker && sudo systemctl start docker &&  \
-    sudo containerd config default > config.toml && \
-    sudo cp config.toml /etc/containerd/config.toml && \
-    sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/' /etc/containerd/config.toml && \
-    sudo sed -i 's/systemd_cgroup \= true/systemd_cgroup \= true/' /etc/containerd/config.toml && \
-    sudo systemctl restart containerd && \
-    sudo setenforce 0 && sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config && \
-    echo -e "[kubernetes]\nname=Kubernetes\nbaseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/\nenabled=1\ngpgcheck=1\ngpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key\nexclude=kubelet kubeadm kubectl cri-tools kubernetes-cni" | sudo tee /etc/yum.repos.d/kubernetes.repo && \
-    sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes && \
-    sudo mkdir -p /etc/kubernetes/pki/ && \
-    sudo cp apiserver.crt /etc/kubernetes/pki/apiserver.crt && \
-    sudo cp apiserver.key /etc/kubernetes/pki/apiserver.key && \
-    sudo bash join-cluster.sh && sleep 30 && \
-    echo "tlsCertFile: /etc/kubernetes/pki/apiserver.crt" | sudo tee -a /var/lib/kubelet/config.yaml && \
-    echo "tlsPrivateKeyFile: /etc/kubernetes/pki/apiserver.key" | sudo tee -a /var/lib/kubelet/config.yaml && \
+    sudo containerd config default > config.toml
+    sudo cp config.toml /etc/containerd/config.toml
+    sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/' /etc/containerd/config.toml
+    sudo sed -i 's/systemd_cgroup \= true/systemd_cgroup \= true/' /etc/containerd/config.toml
+    sudo systemctl restart containerd
+    sudo setenforce 0 && sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+    echo -e "[kubernetes]\nname=Kubernetes\nbaseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/\nenabled=1\ngpgcheck=1\ngpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key\nexclude=kubelet kubeadm kubectl cri-tools kubernetes-cni" | sudo tee /etc/yum.repos.d/kubernetes.repo
+    sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+    sudo mkdir -p /etc/kubernetes/pki/
+    sudo cp apiserver.crt /etc/kubernetes/pki/apiserver.crt
+    sudo cp apiserver.key /etc/kubernetes/pki/apiserver.key
+    sudo bash join-cluster.sh && sleep 30
+    echo "tlsCertFile: /etc/kubernetes/pki/apiserver.crt" | sudo tee -a /var/lib/kubelet/config.yaml
+    echo "tlsPrivateKeyFile: /etc/kubernetes/pki/apiserver.key" | sudo tee -a /var/lib/kubelet/config.yaml
     sudo systemctl restart kubelet
 EOF
 
@@ -167,9 +167,9 @@ function install_helm() {
       --output text)
 
   ssh -o StrictHostKeyChecking=no -i "${KEY_NAME}.pem" ec2-user@$master_ip << EOF
-    kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml && \
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && \
-    chmod 700 get_helm.sh && \
+    kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
     ./get_helm.sh && sleep 30
 EOF
 
